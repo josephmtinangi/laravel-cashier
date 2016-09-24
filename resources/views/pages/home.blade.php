d202c5d80841c3821014e4790e2f4afb57a7461a
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-24 19:58:23 +0300

    Create sass structure

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-	i am the home page
+	
+<div class="hero">
+	<div class="hero-content">
+		<h1>Daily Dose of cute animals!</h1>
+		<h2>Only $10/mo</h2>
+		<div class="cta">
+			<p>We promise you won&quot;t regret it.</p>
+			<a href="/subscribe">Subscribe</a>			
+		</div>
+	</div>
+</div>
+
+<section>
+	<div class="container">
+		<div class="section-header">
+			<h2>Latest Animals</h2>
+		</div>
+
+		{{-- POSTS WILL GO HERE --}}
+	</div>
+</section>
+
 @endsection
