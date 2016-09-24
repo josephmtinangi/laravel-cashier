e9d422aaebc543c1401acacc22da8fd1e2b4652b
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-24 16:39:55 +0300

    Create site routes (home, single post), SiteController and its corresponding views

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SiteController extends Controller
+{
+    public function showHome()
+    {
+    	return view('home');
+    }
+
+    public function showPost()
+    {
+    	return view('post');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+/** site routes */
+Route::get('/', 'SiteController@showHome');
+Route::get('{slug}', 'SiteController@showPost');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/** authentication routes */
+
+/** subscription routes */
+
+/** account routes */
