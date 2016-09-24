e87126a24dc0fc7e15d197053d7bf24a53a257d7
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-24 16:24:01 +0300

    Create Post model, seed users and posts table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +12,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // 
+        // clear the current tables
+        DB::table('users')->delete();
+        DB::table('posts')->delete();
+        // seed posts
+        factory(App\Post::class, 8)->create();
+
+        // create a user we can login with
+        User::create([
+        	'name' => 'Joseph Mtinangi',
+        	'email' => 'josephmtinangi@gmail.com',
+        	'password' => bcrypt('password')
+        	]);
     }
 }
