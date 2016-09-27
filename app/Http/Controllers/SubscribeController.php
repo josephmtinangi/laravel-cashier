cd6e72b1e3575bc14bf644c561812bed013c91ef
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-27 17:59:49 +0300

    Form setup

---
+++ app/Http/Controllers/SubscribeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SubscribeController extends Controller
+{
+	/**
+	 * Show the form
+	 * @return [type] [description]
+	 */
+    public function showSubscribe()
+    {
+    	return view('pages.subscribe');
+    }
+
+    /**
+     * Process the form
+     */
+    public function processSubscribe(Request $request)
+    {
+
+    }
+}
