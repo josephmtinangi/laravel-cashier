891bbd063d3bd1b0a50f9e493447e2e9af7a3858
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-27 18:53:32 +0300

    Stripe setup

--- resources/views/pages/subscribe.blade.php
+++ resources/views/pages/subscribe.blade.php
@@ -11,6 +11,8 @@
 
 <section class="container">
 	<div class="card card-padded">
+
+	<form action="/subscribe" method="POST" id="subscribe-form">
 		
 		@if (Auth::guest())
 		{{-- only show if not logged in --}}
@@ -86,25 +88,25 @@
 				{{-- credit card number --}}
 				<div class="col-xs-8">
 					<label for="">Credit Card Number</label>
-					<input type="text" class="form-control" placeholder="4242 4242 4242 4242">
+					<input type="text" class="form-control" placeholder="4242 4242 4242 4242" data-stripe="number">
 				</div>
 
 				{{-- cvc --}}
 				<div class="col-xs-4">
 					<label>CVC</label>
-					<input type="text" class="form-control" placeholder="123">
+					<input type="text" class="form-control" placeholder="123" data-stripe="cvc">
 				</div>
 			</div>
 		<div class="form-group row">
 			{{-- exp month --}}
 			<div class="col-xs-3">
 				<label>Expiration Month</label>
-				<input type="text" class="form-control" placeholder="08">
+				<input type="text" class="form-control" placeholder="08" data-stripe="exp-month">
 			</div>
 			{{-- exp year --}}
 			<div class="col-xs-3">
 				<label>Expiration Year</label>
-				<input type="text" class="form-control" placeholder="2020">
+				<input type="text" class="form-control" placeholder="2020" data-stripe="exp-year">
 			</div>
 		</div>
 
@@ -113,6 +115,8 @@
 				Subscribe
 			</button>
 		</div>
+
+	</form>	
 
 	</div>
 </section>
