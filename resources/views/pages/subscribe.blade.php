a67344e3b2ce5a4c3e1edec88e76fda399174dd8
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-27 18:10:19 +0300

    Form User Info

--- resources/views/pages/subscribe.blade.php
+++ resources/views/pages/subscribe.blade.php
@@ -12,11 +12,26 @@
 <section class="container">
 	<div class="card card-padded">
 		
+		@if (Auth::guest())
 		{{-- only show if not logged in --}}
 		{{-- user info --}}
 		<div class="section-header">
 			<h2>User Info</h2>
 		</div>
+
+		<div class="form-group">
+			<label for="name">Name</label>
+			<input type="text" name="name" id="name" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="email" name="email" id="email" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" name="password" id="password" class="form-control">
+		</div>
+		@endif
 
 		{{-- subscription info --}}
 		<div class="section-header">
