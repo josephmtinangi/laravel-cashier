1e83e130c63c3aee8e6abff51e6c22c4fad4ecc7
Joseph Mtinangi <josephmtinangi@gmail.com>
2016-09-29 02:01:14 +0300

    Fix factory bug

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -26,11 +26,13 @@ $factory->define(App\Post::class, function(Faker\Generator $faker) {
 	$title = $faker->realText(20);
 	$slug = str_slug($title, '-');
 
+	$pics = ['abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife', 'fashion', 'people', 'nature', 'sports', 'technics', 'transport'];
+
 	return [
 		'user_id' => function() { return factory(App\User::class)->create()->id; },
 		'title' => $title,
 		'slug' => $slug,
-		'image' => $faker->imageUrl(1200, 600, rand('abstract', 'animals', 'business', 'cats', 'city', 'food', 'nightlife', 'fashion', 'people', 'nature', 'sports', 'technics', 'transport')),
+		'image' => $faker->imageUrl(1200, 600, $pics[rand(0, 12)]),
 		'content' => $faker->paragraphs(10, true),
 		'premium' => rand(0, 1)
 	];
